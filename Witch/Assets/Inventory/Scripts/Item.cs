2c64cc2c4c23ce1f790afec0429e6c29c25b8420
Matthew <matybon95@gmail.com>
2020-02-25 19:30:25 +1100

    Started basic UI for game
    Made both inventory and item script
    - Started UI camera and sprite for the UI
    - Debug log tested basic functionality on start
    - Made a new scene called "Matt's Scene" for testing

---
+++ Witch/Assets/Inventory/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{
+    public enum ItemType
+    {
+        potionA,
+        potionB,
+        potionC,
+    }
+
+    public ItemType itemType;
+    public int amount;
+
+}
