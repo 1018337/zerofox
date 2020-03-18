379e38db062515ca7e8cfd75414a23ccc5e5fd20
Matthew <matybon95@gmail.com>
2020-03-18 14:20:33 +1100

    Adding most recent version to my Isolated

---
+++ Witch 0.5.1/Assets/Scripts/Inventories/Inventory/inventoryManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventoryManager : MonoBehaviour
+{
+    #region Singleton
+    public static inventoryManager INSTANCE;
+
+    private void Awake()
+    {
+        INSTANCE = this;
+    }
+    #endregion
+
+    public GameObject slotPrefab;
+    public List<containerGetter> containers = new List<containerGetter>();    
+    private container currentOpenContainer;
+    private itemStack currentDraggedItem = itemStack.Empty;
+    private GameObject spawnedDragStack;
+    private inventoryDraggedItem dragItem;
+    private player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<player>();
+        dragItem = GetComponentInChildren<inventoryDraggedItem>();
+    }    
+
+    public GameObject getContainerPrefab(string name)
+    {
+        foreach(containerGetter container in containers)
+        {
+            if(container.containerName == name)
+            {
+                return container.containerPrefab;
+            }
+        }
+
+        return null;
+    }
+
+
+    public void openContainer(container container, int sizeH)
+    {
+        if(currentOpenContainer != null)
+        {
+            currentOpenContainer.closeContainer();
+        }
+
+        currentOpenContainer = container;
+    }
+
+    public void closeContainer()
+    {
+        if(currentOpenContainer != null)
+        {
+            currentOpenContainer.closeContainer();
+        }
+    }
+
+    public itemStack getDraggedItem()
+    {
+        return currentDraggedItem;
+    }
+
+    public void setDragged(itemStack itemIn)
+    {
+        dragItem.setDraggedItem(currentDraggedItem = itemIn);
+    }
+}
+
+[System.Serializable]
+public class containerGetter
+{
+    public string containerName;
+    public GameObject containerPrefab;
+}
