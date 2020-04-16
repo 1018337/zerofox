48c9ffec6af3fa8ec9891ff07714cb20660c28de
Matthew <matybon95@gmail.com>
2020-04-16 11:04:46 +1000

    Witch 0.6
    Made a splash screen
    Overhauled the main menu with title and a different background.
    Fixed pause menu

---
+++ Witch 0.6/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        //Loads the next scene in the que
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
