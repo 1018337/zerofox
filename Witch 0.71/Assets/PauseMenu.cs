eaa83494f86845b93db7e484522fb74d1bdeb51f
Rick <1018337@student.sae.edu.au>
2020-04-23 16:06:24 +1000

    0.71 bugfixing and final staging prep
    Fixed all the bg items, main game area is playable, fixed a gui issue

---
+++ Witch 0.71/Assets/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameisPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // stops the pause menu from opening up as soon as the game starts
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name != "Menu")
+        {
+            Resume();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game...");
+        Application.Quit();
+    }
+}
