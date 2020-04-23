e7f9e0a1deda80ab0b7647da4945404b7406cc82
Matthew <matybon95@gmail.com>
2020-04-23 17:04:20 +1000

    MixThatWitch 1.1
    Added chest and mixer tutorial in main menu scene after feedback.
    
    Now is much clearer how to play our game

---
+++ MixThatWitch 1.1/Assets/Scripts/Inventories/Inventory/inventory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory
+{
+    private List<itemStack> inventoryContents = new List<itemStack>();
+
+    public inventory(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            inventoryContents.Add(new itemStack(i));
+        }
+    }
+
+    public bool addItem(itemStack input)
+    {
+        foreach(itemStack stack in inventoryContents)
+        {
+            if(stack.isEmpty())
+            {
+                stack.setStack(input);
+                return true;
+            }
+            else
+            {
+                if (itemStack.areItemsEqual(input, stack))
+                {
+                    if(stack.canAddTo(input.getCount()))
+                    {
+                        stack.increaseAmount(input.getCount());
+                        return true;
+                    }
+                    else
+                    {
+                        int difference = (stack.getCount() + input.getCount()) - stack.getItem().maxStack;
+                        stack.setCount(stack.getItem().maxStack);
+                        input.setCount(difference);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public void remAll()
+    {
+        foreach (itemStack stack in inventoryContents)
+        {
+            if (!stack.isEmpty())
+            {
+                stack.decreaseAmount(1);
+            }
+        }
+    }
+    public itemStack getStackInSlot(int index)
+    {
+        return inventoryContents[index];
+    }
+    // return entire content of inventory
+    public List<itemStack> getInventoryStacks()
+    {
+        return inventoryContents;
+    }
+}
---
+++ MixThatWitch 1.1/Assets/Scripts/Inventories/Inventory/inventoryDraggedItem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class inventoryDraggedItem : MonoBehaviour
+{
+    public Image itemIcon;
+    public Text itemAmount;
+
+    public Transform target;
+    public Camera cam;
+
+    private itemStack myStack = itemStack.Empty;
+
+    public void setDraggedItem(itemStack stackIn)
+    {
+        myStack = stackIn;
+    }
+
+    private void drawItem()
+    {
+
+        if (!myStack.isEmpty())
+        {
+            itemIcon.enabled = true;
+            itemIcon.sprite = myStack.getItem().itemIcon;
+            this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(90,90);
+
+            if (myStack.getCount() > 1)
+            {
+                //Debug.Log(myStack.getItem().itemIcon);
+                itemAmount.text = myStack.getCount().ToString();
+            }
+            else
+            {
+                //Debug.Log("draw stack");
+                itemAmount.text = string.Empty;
+            }
+        }
+        else
+        {
+            disableDragItem();
+        }
+    }
+
+    private void disableDragItem()
+    {
+        itemIcon.enabled = false;
+        itemAmount.text = string.Empty;
+    }
+    /*
+    private void Update()
+    {
+        drawItem();
+        transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y , 1);
+        Debug.Log(transform.position);
+    }
+    */
+
+
+    void Update()
+    {
+        drawItem();
+        //Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = new Vector3(pos.x,pos.y, 1);
+
+        // fixed screen point for camera and dragged item
+        var screenPoint = Input.mousePosition;
+        screenPoint.z = 100.0f; //distance of the plane from the camera
+        transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+    }
+}
