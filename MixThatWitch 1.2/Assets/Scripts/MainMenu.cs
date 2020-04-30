3794b8347fd56500cc23486ae4b28f6f86c10bf1
Matthew <matybon95@gmail.com>
2020-04-30 17:10:49 +1000

    MixThatWitch 1.2
    Added Anany's dialouge prompts to game

---
+++ MixThatWitch 1.2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        //Loads the next scene in the que
+        SceneManager.LoadScene("Dialogue");
+    }
+
+    public void QuitGame ()
+    {
+        //Debug log to show that the quit button works in unity without building the game
+        Debug.Log("QUIT!");
+        //This quits the game 
+        Application.Quit();
+    }
+}
---
+++ MixThatWitch 1.2/Assets/Scripts/NovelPrompts/Testing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+
+    DialogueCore dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = DialogueCore.instance;
+    }
+
+
+    public string[] s = new string[]
+    {
+
+    };
+
+
+    int index = 0;
+    bool runOnce = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) || !runOnce)
+        {
+            if (!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+            {
+                if (index >= s.Length)
+                {
+                    return;
+                }
+                Say(s[index]);
+                index++;
+                runOnce = true;
+            }
+        }
+    }
+
+    void Say(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1] : "";
+
+        dialogue.Say(speech, speaker);
+    }
+
+
+}
